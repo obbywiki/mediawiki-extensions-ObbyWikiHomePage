234450e0394460a929a99651391fc8641b11f861
Wolfite <100052562+wlft@users.noreply.github.com>
2026-07-23 01:40:07 +1000

    feat: improve trending articles section

--- src/Hooks.php
+++ src/Hooks.php
@@ -1974,7 +1974,7 @@ SVG;
 			$trendingHTML = '<section class="obbywiki-trending" aria-label="Trending">' .
 				'<div class="obbywiki-trending__header">' .
 					'<span class="obbywiki-trending__icon"><svg xmlns="http://www.w3.org/2000/svg" height="14" viewBox="0 -960 960 960" fill="currentColor"><path d="m136-240-56-56 296-298 160 160 208-206H640v-80h240v240h-80v-104L536-320 376-480 136-240Z"/></svg></span>' .
-					'<h2 class="obbywiki-trending__title">Trending</h2>' .
+					'<h2 class="obbywiki-trending__title">Trending this week</h2>' .
 				'</div>' .
 				'<div class="obbywiki-trending__grid">' . $trendingListHTML . '</div>' .
 			'</section>';
